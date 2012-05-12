bcaa1fa3954d5dd712cf311a82bc5ce0278310c8
Benjamin Fagin <blouis@unquietcode.com>
2012-05-12 13:05:23 -0500

    Copyright headers for everyone!

--- src/main/java/unquietcode/tools/flapi/generator/AbstractGenerator.java
+++ src/main/java/unquietcode/tools/flapi/generator/AbstractGenerator.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ Copyright 2012 Benjamin Fagin
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+
+
+     Read the included LICENSE.TXT for more information.
+ ******************************************************************************/
+
 package unquietcode.tools.flapi.generator;
 
 import com.sun.codemodel.*;
@@ -236,11 +255,11 @@ public abstract class AbstractGenerator<_InType extends Outline, _OutType> imple
 	}
 
 	public JDefinedClass getSubsetInterface(BlockOutline block, Set<MethodOutline> methods) {
-		return getInterface(ctx.getGeneratedName(block.getName()+"Builder", methods));
+		return getInterface(ctx.getGeneratedName(block.getName() + "Builder", methods));
 	}
 
 	public JDefinedClass getSubsetImplementation(BlockOutline block, Set<MethodOutline> methods) {
-		return getClass(ctx.getGeneratedName("Impl"+block.getName()+"Builder", methods));
+		return getClass(ctx.getGeneratedName("Impl" + block.getName() + "Builder", methods));
 	}
 
 	public JDefinedClass getBaseImplementation(BlockOutline block) {
