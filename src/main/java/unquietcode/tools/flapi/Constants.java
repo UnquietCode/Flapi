00995091c4f6015be66ea0309fe31f260d161302
Benjamin Fagin <blouis@unquietcode.com>
2013-10-18 20:51:22 -0700

    finally.
    [FLAPI-170]

--- src/main/java/unquietcode/tools/flapi/Constants.java
+++ src/main/java/unquietcode/tools/flapi/Constants.java
@@ -19,6 +19,9 @@
 
 package unquietcode.tools.flapi;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 /**
  * @author Ben Fagin
  * @version 04-28-2012
@@ -29,7 +32,11 @@ public final class Constants {
 	private Constants() { }
 
 	public static final String PROJECT_URL = "https://github.com/UnquietCode/Flapi";
-	public static final String PROJECT_VERSION = "0.5";     // TODO I hate this. Can we get it from the build?
 	public static final String RETURN_TYPE_NAME = "_ReturnType";
 	public static final String HELPER_VALUE_NAME = "_helper";
+
+	public static final String PROJECT_VERSION; static {
+		InputStream is = Constants.class.getClassLoader().getResourceAsStream("version/version.txt");
+		PROJECT_VERSION = new Scanner(is).useDelimiter("\\A").next();
+	}
 }
