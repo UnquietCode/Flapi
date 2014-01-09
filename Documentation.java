009724ecb344bcca45891f903456971781752de4
Benjamin Fagin <blouis@unquietcode.com>
2014-01-08 23:40:15 -0800

    [FLAPI-122] undo

--- Documentation.java
+++ Documentation.java
@@ -1,10 +1,3 @@
-/*********************************************************************
- Flapi, the fluent API builder for Java.
- Visit the project page at https://github.com/UnquietCode/Flapi
-
- Flapi is free and open software provided without a license.
- ********************************************************************/
-
 // # Flapi
 // ### _A fluent API generator for Java_
 
